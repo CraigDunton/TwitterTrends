248a86b18dbe164f602a49e74579269bd9f325aa
ddamico125 <ddamico.125@gmail.com>
2018-01-13 17:06:24 -0400

    noidea

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-$(".Box").css("background-color");
 
 
-<<<<<<< HEAD
-=======
+$(".click").css("background-color","black");
+
+
 console.log("Whats crackin");
 
 $(document).ready(function() {
     console.log("jquery is  running my guy");
 });
->>>>>>> 9dd191a4778d64731fa8194a28670cc3123c324d
